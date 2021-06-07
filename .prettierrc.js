631ac07697677f72aa50ad618a9169d8a464f5f4
Maxim <maxim.matsuk@clockwork.com.tr>
2021-06-07 17:36:02 +0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
+};
---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+import Background from './background.jpeg';
+
+export { Background };
